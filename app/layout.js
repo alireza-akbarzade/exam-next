629e20b0808df34d56ee07cd62f979eeefd422d3
alireza <alireza.akbarzade88@gmail.com>
2026-05-11 20:58:46 +0330

    add Navbar

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,6 @@
 import { Inter } from 'next/font/google'
 import Footer from './components/layout/Footer'
+import Navbar from './components/layout/Navbar'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -28,7 +29,7 @@ export default function RootLayout({ children }) {
       </head>
       <body className={inter.className}>
 
-       
+       <Navbar/>
         
         {children}
 
