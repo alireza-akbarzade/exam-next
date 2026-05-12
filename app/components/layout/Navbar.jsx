2ccc3b3f92ef8362ca1a581b8de66403b481852d
alireza <alireza.akbarzade88@gmail.com>
2026-05-12 19:15:19 +0330

    add active link

--- app/components/layout/Navbar.jsx
+++ app/components/layout/Navbar.jsx
@@ -1,7 +1,12 @@
+'use client'
 import Link from 'next/link';
+import { usePathname } from 'next/navigation';
 import React from 'react';
 
 const Navbar = () => {
+// ادرس اون صفحه
+  const pathName =usePathname()
+
     return (
         <div className="top-area">
         <div className="header-area">
@@ -18,10 +23,10 @@ const Navbar = () => {
               </div>
               <div className="collapse navbar-collapse menu-ui-design" id="navbar-menu">
                 <ul className="nav navbar-nav navbar-right" data-in="fadeInDown" data-out="fadeOutUp">
-                  <li className=" scroll active"><Link href="/">home</Link></li>
-                  <li className="scroll"><Link href="/services">service</Link></li>
+                  <li className=" scroll"><Link   style={{color:pathName=== '/'? "red":''}}  href="/"  >home</Link></li>
+                  <li className="scroll"><Link href="/services"  style={{color:pathName=== '/services' ? "red":''}} >service</Link></li>
                   <li className="scroll"><Link href="#featured-cars">featured cars</Link></li>
-                  <li className="scroll"><Link href="/cars">new cars</Link></li>
+                  <li className="scroll"><Link href="/cars"  style={{color:pathName=== '/cars' ? "red":''}} >new cars</Link></li>
                   <li className="scroll"><Link href="#brand">brands</Link></li>
                   <li className="scroll"><Link href="#contact">contact</Link></li>
                 </ul>
